2da72555d5fdd36b63e6bea102a49b15452f47b0
SergiVilardell <sergivilardell13@gmail.com>
2018-06-06 18:58:11 +0200

    Add stuff

--- sandbox.R
+++ sandbox.R
@@ -1,8 +1,27 @@
 library(tidyverse)
 library(data.table)
+library(corrplot)
+library(viridis)
 
 #Read data
-lifetime_df <- fread("Courier_lifetime_data.csv", sep = ",", header= TRUE)
-weekly_df <- fread("Courier_weekly_data.csv", sep = ",", header= TRUE)
+lifetime.df <- fread("Courier_lifetime_data.csv", sep = ",", header= TRUE)
+weekly.df <- fread("Courier_weekly_data.csv", sep = ",", header= TRUE)
+
+
+#Correlation matrix and plot
+corr.matrix <- round(cor(weekly.df[,-(1:2)]), 2)
+
+corrplot(corr.matrix, method = "color", col = viridis(200),
+		 type = "upper", order = "hclust", number.cex = .7,
+		 addCoef.col = "black", 
+		 tl.col = "black",
+		 diag = FALSE)
+
+
+#Add lifetime feature_1 into weekly 
+
+a <-  which(lifetime.df[["courier"]] == weekly.df[["courier"]])
+b <- lifetime.df[ids,2]
+weekly.df[["feature_18"]] <- b
 
 
