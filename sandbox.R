ebe186c3c1ed7f585d22243e6a9f9d0123b50ff2
SergiVilardell <sergivilardell13@gmail.com>
2018-06-06 23:47:50 +0200

    praise hadley

--- sandbox.R
+++ sandbox.R
@@ -2,15 +2,38 @@ library(tidyverse)
 library(data.table)
 library(corrplot)
 library(viridis)
+library(GGally)
 
 #Read data
 lifetime.df <- fread("Courier_lifetime_data.csv", sep = ",", header= TRUE)
 weekly.df <- fread("Courier_weekly_data.csv", sep = ",", header= TRUE)
 
+#Add lifetime feature_1 into weekly 
+ids <-  match(weekly.df[["courier"]], lifetime.df[["courier"]])
+b <- as.vector(lifetime.df[ids,2])
+weekly.df$feature_18 <- b
+
+
 
 #Correlation matrix and plot
 corr.matrix <- round(cor(weekly.df[,-(1:2)]), 2)
 
+ggplot(lifetime.df[lifetime.df$feature_2 %in% -10:100 ], aes(x = feature_2))+
+  geom_histogram(binwidth = 1)+
+  facet_wrap(~feature_1)
+
+ggplot(lifetime.df, aes(x = feature_1))+
+  geom_bar()
+
+  
+  
+  
+plot(lifetime.df$feature_2[lifetime.df$feature_2 %in% -10:100 ], col = as.factor(lifetime.df$feature_1))
+ggpairs(weekly.df[, c(3:10)], aes(colour = weekly.df$feature_18, alpha = 0.5))
+
+
+
+#PRAISE HADLEY
 corrplot(corr.matrix, method = "color", col = viridis(200),
 		 type = "upper", order = "hclust", number.cex = .7,
 		 addCoef.col = "black", 
@@ -18,10 +41,5 @@ corrplot(corr.matrix, method = "color", col = viridis(200),
 		 diag = FALSE)
 
 
-#Add lifetime feature_1 into weekly 
-
-a <-  which(lifetime.df[["courier"]] == weekly.df[["courier"]])
-b <- lifetime.df[ids,2]
-weekly.df[["feature_18"]] <- b
 
 
