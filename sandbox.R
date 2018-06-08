42938fddccd7c46c47cb48000e6ae60929c397b8
SergiVilardell <sergivilardell13@gmail.com>
2018-06-08 10:43:23 +0200

    beh

--- sandbox.R
+++ sandbox.R
@@ -14,31 +14,32 @@ b <- as.vector(lifetime.df[ids,2])
 weekly.df$feature_18 <- b
 
 
-
 #Correlation matrix and plot
-corr.matrix <- round(cor(weekly.df[,-(1:2)]), 2)
+#corr.matrix <- round(cor(weekly.df[,-(1:2)]), 2)
 
-ggplot(lifetime.df[lifetime.df$feature_2 %in% -10:100 ], aes(x = feature_2))+
-  geom_histogram(binwidth = 1)+
-  facet_wrap(~feature_1)
+#ggplot(lifetime.df[lifetime.df$feature_2 %in% -10:100 ], aes(x = feature_2))+
+#  geom_histogram(binwidth = 1)+
+#  facet_wrap(~feature_1)
 
-ggplot(lifetime.df, aes(x = feature_1))+
-  geom_bar()
+#ggplot(lifetime.df, aes(x = feature_1))+
+#  geom_bar()
 
   
   
   
-plot(lifetime.df$feature_2[lifetime.df$feature_2 %in% -10:100 ], col = as.factor(lifetime.df$feature_1))
-ggpairs(weekly.df[, c(3:10)], aes(colour = weekly.df$feature_18, alpha = 0.5))
+#plot(lifetime.df$feature_2[lifetime.df$feature_2 %in% -10:100 ], col = as.factor(lifetime.df$feature_1))
+
+
+###PRAISE HADLEY
+#ggpairs(weekly.df[, c(3:10)], aes(colour = weekly.df$feature_18, alpha = 0.5))
 
 
 
-#PRAISE HADLEY
-corrplot(corr.matrix, method = "color", col = viridis(200),
-		 type = "upper", order = "hclust", number.cex = .7,
-		 addCoef.col = "black", 
-		 tl.col = "black",
-		 diag = FALSE)
+#corrplot(corr.matrix, method = "color", col = viridis(200),
+#		 type = "upper", order = "hclust", number.cex = .7,
+#		 addCoef.col = "black", 
+#		 tl.col = "black",
+#		 diag = FALSE)
 
 
 
